9a4091fc7e28d56bbedd97fb63bca0b9e9a68c20
Martin <stupidswan93@gmail.com>
2021-01-07 13:22:12 +0800

    a

--- components/gesture-handler.js
+++ components/gesture-handler.js
@@ -3,7 +3,7 @@
 AFRAME.registerComponent("gesture-handler", {
     schema: {
       enabled: { default: true },
-      rotationFactor: { default: 50 },
+      rotationFactor: { default: 5 },
       minScale: { default: 0.3 },
       maxScale: { default: 8 },
     },
