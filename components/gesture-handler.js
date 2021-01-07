bb42d109a23c1e655399c3cb262645a038aea104
Martin <stupidswan93@gmail.com>
2021-01-07 14:36:49 +0800

    a

--- components/gesture-handler.js
+++ components/gesture-handler.js
@@ -34,7 +34,7 @@ AFRAME.registerComponent("gesture-handler", {
   
     handleRotation: function (event) {
       if (this.isVisible) {
-        this.el.object3D.rotation.x +=
+        this.el.object3D.rotation.y +=
           event.detail.positionChange.y * this.data.rotationFactor;
         // this.el.object3D.rotation.x +=
         //   event.detail.positionChange.y * this.data.rotationFactor;
