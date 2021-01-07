a1cdccc26c671f863e0622e0b0481d32334f6d0e
Martin <stupidswan93@gmail.com>
2021-01-07 14:14:32 +0800

    a

--- components/gesture-handler.js
+++ components/gesture-handler.js
@@ -18,15 +18,21 @@ AFRAME.registerComponent("gesture-handler", {
     },
   
     update: function () {
-        this.el.addEventListener("onefingermove", this.handleRotation);
-        this.el.addEventListener("twofingermove", this.handleScale);
-        console.log(this);
-        console.log(this.el);
+      if (this.data.enabled) {
+        this.el.sceneEl.addEventListener("onefingermove", this.handleRotation);
+        this.el.sceneEl.addEventListener("twofingermove", this.handleScale);
+      } else {
+        this.el.sceneEl.removeEventListener("onefingermove", this.handleRotation);
+        this.el.sceneEl.removeEventListener("twofingermove", this.handleScale);
+      }
+      console.log(this);
+      console.log(this.data);
+      console.log(this.el);
     },
   
     remove: function () {
-      this.el.removeEventListener("onefingermove", this.handleRotation);
-      this.el.removeEventListener("twofingermove", this.handleScale);
+      this.el.sceneEl.removeEventListener("onefingermove", this.handleRotation);
+      this.el.sceneEl.removeEventListener("twofingermove", this.handleScale);
     },
   
     handleRotation: function (event) {
